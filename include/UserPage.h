7cd9686a772ecdfd1a9f46a12f6faf725fe36470
M Rayandra Erlangga <muhamadrayandra741@gmail.com>
2026-05-02 19:24:29 +0800

    menu user dan userpage.h

---
+++ include/UserPage.h
@@ -0,0 +1,14 @@
+#ifndef USERPAGE_H
+#define USERPAGE_H
+
+#include "Utils.h"
+#include "Models.h"
+#include "Auth.h"
+
+void menuUser();
+void preRegister();
+void cekJadwal();
+void riwayatDonor();
+void edukasi();
+
+#endif // USERPAGE_H
